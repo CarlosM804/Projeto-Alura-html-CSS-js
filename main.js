5d4db1e57a28e90376d7867969b7f5b690e6b6f2
carlos <miguelcarloss804@gmail.com>
2022-04-07 22:10:39 -0300

    primeira versão

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+let forma = 'retângulo';
+let altura = 5;
+let comprimento = 7;
+console.log(comprimento);
+
+ if (forma === 'retângulo'){
+    let area = altura + comprimento; 
+    console.log(resultadoRetângulo);
+  } else{
+        area = altura * comprimento /2
+    }
+ 
